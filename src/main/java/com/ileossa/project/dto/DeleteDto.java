ef5442645f3873df539b958945d86c361ad72f30
ileossa <vincnet.iam@gmail.com>
2017-08-03 21:38:03 +0200

    Add dto for delete user account

---
+++ src/main/java/com/ileossa/project/dto/DeleteDto.java
@@ -0,0 +1,20 @@
+package com.ileossa.project.dto;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by ileossa on 01/08/2017.
+ */
+public class DeleteDto {
+
+    @NotNull
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
