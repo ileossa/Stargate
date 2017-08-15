bb05b9bf834dc040fceaf3afd0bf8d9b4eacc6c6
ileossa <vincnet.iam@gmail.com>
2017-08-15 15:55:11 +0200

    forget commit uploadDao

---
+++ src/main/java/com/ileossa/project/uploadFiles/dao/File.java
@@ -0,0 +1,64 @@
+package com.ileossa.project.uploadFiles.dao;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by ileossa on 14/08/2017.
+ */
+@Entity
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    long id;
+
+    private String name;
+
+    private String tag;
+
+    private String classpath;
+
+    public File() {
+    }
+
+    public File(String name, String tag, String classpath) {
+        this.name = name;
+        this.tag = tag;
+        this.classpath = classpath;
+    }
+
+    public String getClasspath() {
+        return classpath;
+    }
+
+    public void setClasspath(String classpath) {
+        this.classpath = classpath;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
