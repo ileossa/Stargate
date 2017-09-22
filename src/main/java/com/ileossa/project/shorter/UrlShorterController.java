1f08fb19595672070bdd3db7e9659b2fd76e6f41
ileossa <vincnet.iam@gmail.com>
2017-09-22 14:12:38 +0200

    optimize dependencies

--- src/main/java/com/ileossa/project/shorter/UrlShorterController.java
+++ src/main/java/com/ileossa/project/shorter/UrlShorterController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
--- src/test/java/com/ileossa/project/shorter/UrlShorter.java
+++ src/test/java/com/ileossa/project/shorter/UrlShorter.java
@@ -1,7 +1,5 @@
 package com.ileossa.project.shorter;
 
-import com.ileossa.project.email.config.EmailConfig;
-import com.ileossa.project.mail.impl.EmailServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
